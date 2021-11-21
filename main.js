5245290b0c6bc79fa3a4b12c04ffcddd52cff921
Vaishnawik05 <79893634+Vaishnawik05@users.noreply.github.com>
2021-11-21 08:44:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-function preload(){
+lipX=0;
+lipY=0;
 
+function preload(){
+lip_stick = loadImage('https://i.postimg.cc/PxFvYgkv/l1.png');
 }
 
 function setup(){
@@ -22,12 +25,15 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
-        console.log("nose x = " + results[0].pose.nose.x);
-        console.log("nose y = " + results[0].pose.nose.y);
+        lipX = results[0].pose.nose.x;
+        lipY = results[0].pose.nose.y;
+        console.log("lip x = " + lipX);
+        console.log("lip y = " + lipY);
     }
 }
 function draw(){
     image(video, 0, 0, 300, 300);
+    image(lip_stick, lipX, lipY, 40, 40);
 }
 
 function take_snapshot(){
